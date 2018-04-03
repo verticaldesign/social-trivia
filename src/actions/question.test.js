a07a6226abecfd437d8db47cdbb7d4d6aef550cc
ccalbaugh <cadenalbaugh@gmail.com>
2018-04-03 15:19:42 -0500

    test: add mockStore to test specs

--- src/actions/question.test.js
+++ src/actions/question.test.js
@@ -1,14 +1,32 @@
 import 'isomorphic-fetch';
 import { expect } from 'code';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import * as actions from './question';
 import * as types from './actionTypes';
 
 const currentQuestion = "How much?";
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
 
-it('creates an action to update the currentQuestion', () => {
+let store
+
+beforeEach(() => {
+    store = mockStore({})
+})
+
+afterEach(() => {
+    store.clearActions()
+})
+
+it('creates an async action to update the currentQuestion', () => {
 
     const expectedAction = { type: types.UPDATE_CURRENT_QUESTION, currentQuestion };
 
-    expect(actions.updateCurrentQuestion(currentQuestion)).to.equal(expectedAction);
+    return store.dispatch(actions.updateCurrentQuestion(currentQuestion))
+        .then(() => {
+            const storeActions = store.getActions();
+            expect(storeActions[0]).to.equal(expectedAction);
+        })
 
 });
