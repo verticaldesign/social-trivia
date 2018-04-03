e3c132e99ca98ce160bdcbb3a28f8d48e3f520e8
ccalbaugh <cadenalbaugh@gmail.com>
2018-04-03 15:20:28 -0500

    test: add tests to reflect updated code behavior

--- src/actions/timer.test.js
+++ src/actions/timer.test.js
@@ -9,7 +9,7 @@ import * as types from './actionTypes';
 const middlewares = [thunk]
 const mockStore = configureStore(middlewares)
 
-let store
+let store;
 
 beforeEach(() => {
     store = mockStore({})
@@ -19,50 +19,70 @@ afterEach(() => {
     store.clearActions()
 })
 
-it('creates an action to create the timer', () => {
+it('creates an async action to create the timer', () => {
 
     const expectedAction = { type: types.CREATE_TIMER, isTimerRunning: false };
 
-    expect(actions.createTimer()).to.equal(expectedAction)
+    return store.dispatch(actions.createTimer())
+        .then(() => {
+            const storeActions = store.getActions();
+            expect(storeActions[0]).to.equal(expectedAction);
+        })
 
 });
 
-it('creates an action to control the timer', () => {
+it('creates an async action to control the timer', () => {
 
     const mockIsTimerRunning = true;
 
     const expectedAction = { type: types.CONTROL_TIMER, isTimerRunning: mockIsTimerRunning };
 
-    expect(actions.controlTimer(mockIsTimerRunning)).to.equal(expectedAction)
+    return store.dispatch(actions.controlTimer(mockIsTimerRunning))
+        .then(() => {
+            const storeActions = store.getActions();
+            expect(storeActions[0]).to.equal(expectedAction);
+        })
 
 });
 
-it('creates an action to reset the timer', () => {
+it('creates an async action to reset the timer', () => {
 
     const mockDefaultTime = 10;
 
     const expectedAction = { type: types.RESET_TIMER, currentTime: mockDefaultTime };
 
-    expect(actions.resetTimer(mockDefaultTime)).to.equal(expectedAction)    
+    return store.dispatch(actions.resetTimer(mockDefaultTime))
+        .then(() => {
+            const storeActions = store.getActions();
+            expect(storeActions[0]).to.equal(expectedAction);
+        })
 
 });
 
-it('creates an action to update the timer', () => {
+it('creates an async action to update the timer', () => {
 
     const mockSetTime = 10;
 
     const expectedAction = { type: types.UPDATE_TIMER, setTime: mockSetTime };
 
-    expect(actions.updateTimer(mockSetTime)).to.equal(expectedAction)    
+    return store.dispatch(actions.updateTimer(mockSetTime))
+        .then(() => {
+            const storeActions = store.getActions();
+            expect(storeActions[0]).to.equal(expectedAction);
+        })  
         
 });
 
-it('creates an action to decrement the timer', () => {
+it('creates an async action to decrement the timer', () => {
 
     const mockTimeLeft = 10;
 
     const expectedAction = { type: types.DECREMENT_TIMER, timeLeft: mockTimeLeft };
 
-    expect(actions.decrementTimer(mockTimeLeft)).to.equal(expectedAction)    
+    return store.dispatch(actions.decrementTimer(mockTimeLeft))
+        .then(() => {
+            const storeActions = store.getActions();
+            expect(storeActions[0]).to.equal(expectedAction);
+        })   
         
 });
