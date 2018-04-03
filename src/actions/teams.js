2a1d4bce89f4103f893ed6344c38b15c73a82331
ccalbaugh <cadenalbaugh@gmail.com>
2018-04-03 14:05:23 -0500

    feat: add async logic to fulfill corrseponding test specs

--- src/actions/teams.js
+++ src/actions/teams.js
@@ -5,18 +5,22 @@ const teams = database.ref('teams/')
 const isShowingAnswersInDB = database.ref('isShowingAnswers')
 
 export function createTeam(id, createdAt) {
-    return {
-        type: types.CREATE_TEAM,
-        id,
-        createdAt
-    };
-}
-
-export function createTeamInDB(id, createdAt) {
     return dispatch => {
-        teams.child(id).set({ answer: 0, answeredAt: 0, score: 0, createdAt, isSubmitted: false })
-        dispatch(createTeam(id, createdAt)) 
-    }
+        return teams.child(id).set({ 
+            answer: 0, 
+            answeredAt: 0, 
+            score: 0, 
+            createdAt, 
+            isSubmitted: false 
+        })
+        .then(() => {
+            dispatch({
+                type: types.CREATE_TEAM,
+                id,
+                createdAt
+            })
+        })
+    };
 }
 
 export function fetchTeams(teams) {
@@ -28,65 +32,81 @@ export function fetchTeams(teams) {
 
 export function fetchTeamsFromDB() {
     return dispatch => {
-      teams.on('value', snapshot => {
-        dispatch(fetchTeams(snapshot.val()))
-      });
-    }
+        teams.on('value', snapshot => {
+            dispatch(fetchTeams(snapshot.val()))
+        });
+    };
 }
 
 export function fetchIsShowingAnswers() {
     return dispatch => {
         isShowingAnswersInDB.on('value', snapshot => {
-            dispatch({ type: types.FETCH_IS_SHOWING_ANSWERS, isShowingAnswers: snapshot.val() })
+            dispatch({ 
+                type: types.FETCH_IS_SHOWING_ANSWERS, 
+                isShowingAnswers: snapshot.val() 
+            })
         })
-    }
+    };
 }
 
 export function submitAnswer(answer, id, answeredAt, isSubmitted) {
-    return {
-        type: types.SUBMIT_ANSWER,
-        answer,
-        id,
-        answeredAt,
-        isSubmitted
-    };
-}
-
-export function submitAnswerToDB(answer, id, answeredAt, isSubmitted) {
-   return dispatch => {
-      teams.child(id).update({ answer, id, answeredAt, isSubmitted })
-      dispatch(submitAnswer(answer, id, answeredAt, isSubmitted))
-   }
-}
-
-export function updateTeam(score, id) {
-    teams.child(id).update({ answer: 0, answeredAt: 0 })
-    return {
-        type: types.UPDATE_TEAM,
-        score,
-        id
-    };
-}
-
-export function submitTeamScoreToDB(currentScore, id, addToScore) {
     return dispatch => {
-        const score = currentScore + addToScore
-        teams.child(id).update({ score: score, isSubmitted: false })
-    }
+        return teams.child(id).update({ 
+            answer, 
+            id, 
+            answeredAt, 
+            isSubmitted 
+        })
+        .then(() => {
+            dispatch({
+                type: types.SUBMIT_ANSWER,
+                answer,
+                id,
+                answeredAt,
+                isSubmitted
+            })
+        })
+    };
+}
+
+export function updateTeam(newScore, id) {
+    return dispatch => {
+        return teams.child(id).update({ 
+            answer: 0, 
+            answeredAt: 0, 
+            score: newScore,
+            isSubmitted: false
+        })
+        .then(() => {
+            dispatch({
+                type: types.UPDATE_TEAM,
+                newScore,
+                id
+            })
+        })
+    };
 }
 
 export function toggleShowAnswers(isShowingAnswers) {
-    isShowingAnswersInDB.set(!isShowingAnswers)
-    return {
-        type: types.TOGGLE_SHOW_ANSWERS,
-        isShowingAnswers
-    }
+    return dispatch => {
+        return isShowingAnswersInDB.set(!isShowingAnswers)
+        .then(() => {
+            dispatch({
+                type: types.TOGGLE_SHOW_ANSWERS,
+                isShowingAnswers
+            })
+        })
+    };
 }
 
 export function deleteTeam(id) {
-    teams.child(id).remove();
-    return {
-        type: types.DELETE_TEAM,    
-        id
+    return dispatch => {
+        return teams.child(id).remove()
+        .then(() => {
+            dispatch({
+                type: types.DELETE_TEAM,    
+                id
+            })
+        })
     };
 }
