d7a1b2b9a2cdbb14a68a7b5e3fce8bec55608708
ccalbaugh <cadenalbaugh@gmail.com>
2018-04-04 09:42:47 -0500

    test: add case for when nothing is passed

--- src/actions/question.test.js
+++ src/actions/question.test.js
@@ -6,6 +6,7 @@ import * as actions from './question';
 import * as types from './actionTypes';
 
 const currentQuestion = "How much?";
+const emptyString = '';
 const middlewares = [thunk]
 const mockStore = configureStore(middlewares)
 
@@ -24,6 +25,18 @@ it('creates an async action to update the currentQuestion', () => {
     const expectedAction = { type: types.UPDATE_CURRENT_QUESTION, currentQuestion };
 
     return store.dispatch(actions.updateCurrentQuestion(currentQuestion))
+        .then(() => {
+            const storeActions = store.getActions();
+            expect(storeActions[0]).to.equal(expectedAction);
+        })
+
+});
+
+it('creates an async action to update the currentQuestion to an empty string if nothing is passed to it', () => {
+
+    const expectedAction = { type: types.UPDATE_CURRENT_QUESTION, currentQuestion: emptyString };
+
+    return store.dispatch(actions.updateCurrentQuestion())
         .then(() => {
             const storeActions = store.getActions();
             expect(storeActions[0]).to.equal(expectedAction);
