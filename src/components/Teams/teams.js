11f5b6acedbddf95e7538974bf6b7fda127903d7
ccalbaugh <cadenalbaugh@gmail.com>
2018-04-04 09:18:20 -0500

    feat: add list animations library

--- src/components/Teams/teams.js
+++ src/components/Teams/teams.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { fetchTeamsFromDB, fetchIsShowingAnswers, updateTeam } from '../../actions/teams'
 import { fetchCurrentQuestionFromDB } from '../../actions/question'
 import Timer from '../Timer/timer'
+import FlipMove from 'react-flip-move'
 import './teams.css';
 
 export class Teams extends Component {
@@ -76,43 +77,45 @@ export class Teams extends Component {
                     </span> 
                 }
                 <ul className="team-list">
-                    {
-                        (!!teams && !!teams.length) ? (
-                            teams.map((team) => {
-                                const submitted = team.isSubmitted ? 'submitted' : '';
-                                const answeredFirst = team.answeredFirst ? 'answered-first' : '';
-                                return (
-                                    <li className={`team-list-item ${submitted} ${answeredFirst}`} 
-                                        key={team.id}
-                                    >
-                                        <div className={'team-name'}>{team.id}</div>
+                    <FlipMove duration={300} easing="ease-out">
+                        {
+                            (!!teams && !!teams.length) ? (
+                                teams.map((team) => {
+                                    const submitted = team.isSubmitted ? 'submitted' : '';
+                                    const answeredFirst = team.answeredFirst ? 'answered-first' : '';
+                                    return (
+                                        <li className={`team-list-item ${submitted} ${answeredFirst}`} 
+                                            key={team.id}
+                                        >
+                                            <div className={'team-name'}>{team.id}</div>
 
-                                        <div className={'label-group team-answer-group'}>
-                                            <label>Team Answer: </label>
-                                            <span className="team-answer">
-                                                {
-                                                    (isShowingAnswers && team.answer) && (`${team.answer}`)
-                                                }
-                                            </span>
-                                        </div>
-                                        <div className={'label-group team-score-group'}>
-                                            <label>Score: </label>
-                                            { parentNotAdmin ? (
-                                                <span className="team-score">{team.score || 0}</span>
-                                            ) : (
-                                                <span className="team-score">
-                                                    <button className="button decrement-team-score-button" onClick={ () => { updateTeam((team.score - 1), team.id) } }>-</button>
-                                                        {team.score || 0}       
-                                                    <button className="button increment-team-score-button" onClick={ () => { updateTeam((team.score + 1), team.id) } }>+</button>    
-                                                </span>                                                                                         
-                                            )}
-                                        </div>
-                                    </li>
-                                )
-                            })
-                        ) :
-                        <li>No Teams</li>
-                    }
+                                            <div className={'label-group team-answer-group'}>
+                                                <label>Team Answer: </label>
+                                                <span className="team-answer">
+                                                    {
+                                                        (isShowingAnswers && team.answer) && (`${team.answer}`)
+                                                    }
+                                                </span>
+                                            </div>
+                                            <div className={'label-group team-score-group'}>
+                                                <label>Score: </label>
+                                                { parentNotAdmin ? (
+                                                    <span className="team-score">{team.score || 0}</span>
+                                                ) : (
+                                                    <span className="team-score">
+                                                        <button className="button decrement-team-score-button" onClick={ () => { updateTeam((team.score - 1), team.id) } }>-</button>
+                                                            {team.score || 0}       
+                                                        <button className="button increment-team-score-button" onClick={ () => { updateTeam((team.score + 1), team.id) } }>+</button>    
+                                                    </span>                                                                                         
+                                                )}
+                                            </div>
+                                        </li>
+                                    )
+                                })
+                            ) :
+                            <li>No Teams</li>
+                        }
+                    </FlipMove>
                 </ul>
             </section>
         )
